22f84817146cd88512ecc67fb46af5ae777c5bda
TiciaChance <laticia.chance@flatironschool.com>
2016-11-08 14:23:11 -0500

    class file and new constraints addition

---
+++ SpotifySearch/AudioVC.swift
@@ -0,0 +1,17 @@
+//
+//  AudioVC.swift
+//  SpotifySearch
+//
+//  Created by Laticia Chance on 11/8/16.
+//  Copyright © 2016 Laticia Chance. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AudioVC : UIViewController {
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
